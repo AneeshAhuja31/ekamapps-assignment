2d58dcdda872d6a0a45cdafca4bf82ec6ceef911
Aneesh Ahuja <aneeshahuja31@gmail.com>
2025-09-27 05:16:40 +0530

    added origin

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ app = FastAPI(title="NLP Query Engine", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ekamapps-assignment.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
