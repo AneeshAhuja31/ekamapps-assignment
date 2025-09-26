d9e90347ceb8570e89046cb44dcdb6577f62498e
Aneesh Ahuja <aneeshahuja31@gmail.com>
2025-09-27 02:34:10 +0530

    port binded

--- main.py
+++ main.py
@@ -211,4 +211,4 @@ async def get_metrics():
     }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT") or 8000, reload=True)
